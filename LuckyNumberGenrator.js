b197a8a0267cdb7caecaaab950700d7c2a181203
Abhinav Singh Bhadauria <abino@Abhinavs-MacBook-Air.local>
2025-05-15 15:04:24 +0530

    update

--- LuckyNumberGenrator.js
+++ LuckyNumberGenrator.js
@@ -1,19 +1,15 @@
 // set your lucky number
-let draw = 9999; // only 0 to 9999
+const draw = 9999; // only 0 to 9999
 
 // main code starts from here
 let n;
 const history = []; // this will store the history
 
-while (true) {
+while (n != draw) {
   n = Math.floor(Math.random() * 10000); // generate random numbers from 0 to 9999
   history.push(n); // add into history
-  if (n == draw) {
-    console.log(`Start -> ${history} <- end`);
-    console.log("-------------------------------------------------------");
-    console.log(`-> History is above this line`);
-    console.log(`-> We got our ticket in ${history.length} times :)`);
-
-    break;
-  }
 }
+
+console.log(`${history}`);
+console.log("-------------------------------------------------------");
+console.log(`-> We got our ticket in ${history.length} times :)`);
